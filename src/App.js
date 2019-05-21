6c1d9c4b5db0057ae2690f5a1d7526c10017d1bb
jwy5140 <jwy5140@gmail.com>
2019-05-21 02:17:11 -0400

    Done.

--- src/App.js
+++ src/App.js
@@ -1,1 +1,29 @@
 // create your App component here
+
+import React from 'react'
+
+export default class App extends React.Component{
+
+    state = {
+        peeps: []
+    }
+
+    componentDidMount(){
+        fetch('http://api.open-notify.org/astros.json')
+        .then(resp=>resp.json())
+        .then(json=>{
+            this.setState({
+                peeps: json.people
+            })
+        })
+    }
+
+    render(){
+        return this.state.peeps.map(peep=>{
+            <div>
+                <p>Name: {peep.name}</p>
+                <p>Spacecraft: {peep.craft}</p>
+            </div>
+        })
+    }
+}
